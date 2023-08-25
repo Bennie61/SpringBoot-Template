31ffd24eaf5fbf1ace92786887dbf04acb3cf532
Bennie61 <bennylu98@163.com>
2023-08-26 00:17:22 +0800

    对用户登录接口进行了简化

--- src/main/java/com/benny/usercenter/service/UserService.java
+++ src/main/java/com/benny/usercenter/service/UserService.java
@@ -3,18 +3,32 @@ package com.benny.usercenter.service;
 import com.benny.usercenter.model.domain.User;
 import com.baomidou.mybatisplus.extension.service.IService;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
-* @author benny
-* @description 针对表【user(用户表)】的数据库操作Service
-* @createDate 2023-08-18 11:18:12
-*/
+ * @author benny
+ * @description 针对表【user(用户表)】的数据库操作Service
+ * @createDate 2023-08-18 11:18:12
+ */
 public interface UserService extends IService<User> {
     /**
-     * 用户注册
-     * @param userAccount  账户
-     * @param userPassword 密码
+     * 用户注册接口
+     * @param userAccount   账户
+     * @param userPassword  密码
      * @param checkPassword 确认密码
      * @return 失败返回-1
      */
-    long userRegister(String userAccount,String userPassword,String checkPassword);
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 用户登录接口
+     * @param userAccount 账户
+     * @param userPassword 密码
+     * @param request 用来处理用户的登录态
+     * @return  脱敏后的用户信息
+     */
+    User doLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    // Note: 鼠标放在接口名xxx上，按ALT+Enter，选择 implement method ‘xxx’,
+    // 快速在对应目录下生成接口的具体实现代码
 }
--- src/main/java/com/benny/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/benny/usercenter/service/impl/UserServiceImpl.java
@@ -1,15 +1,18 @@
 package com.benny.usercenter.service.impl;
+import java.util.Date;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.benny.usercenter.model.domain.User;
 import com.benny.usercenter.service.UserService;
 import com.benny.usercenter.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,14 +21,27 @@ import java.util.regex.Pattern;
 * @description 针对表【user(用户表)】的数据库操作Service实现
 * @createDate 2023-08-18 11:18:12
 */
+@Slf4j
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
     @Resource
     private UserMapper userMapper;
+
+    /*
+        盐值，混淆密码
+     */
+    private static final String SALT = "yupi";
+    // Note：输入 prsf，快速打出常量的类型。
+    /**
+     * 用户登录态键
+     */
+    private static final String USER_LOGIN_STATE = "userLoginState";
+
     @Override
     public long userRegister(String userAccount, String userPassword, String checkPassword) {
-        // 快速测试方法，鼠标放在放在方法上，按Alt+Enter，选择 Generate missed test methods
+        // 快速测试方法，鼠标放在放在方法上，按Alt+Enter，选择 Generate missed test methods，
+        // 会在test相对应的目录下生成测试类
         // 1. 校验
         if(StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)){
             return -1;
@@ -51,7 +67,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         // 账户不能重复
         QueryWrapper<User> queryWrapper = new QueryWrapper<>();
         // https://blog.csdn.net/qq_26383975/article/details/119646390
-        // QueryWrapper就是在使用Mybatis-plus中真实用到的一种技术，也叫作构造器，能简化sql的操作。
+        // QueryWrapper就是在使用 Mybatis-plus 中真实用到的一种技术，也叫作构造器，能简化sql的操作。
         // QueryWrapper其实可以理解成一个放查询条件的盒子，我们把查询条件放在里面，他就会自动按照对应的条件进行查询数据。
         // 构建一个查询的wrapper
         queryWrapper.eq("userAccount",userAccount);
@@ -59,8 +75,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         if(count > 0){
             return -1;
         }
-        // 2. 加密 使用加密工具库 DigestUtils
-        final String SALT = "yupi";
+        // 2. 加密 Note：使用加密工具库 DigestUtils。
         String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
         // 3. 插入数据
         User user = new User();
@@ -72,6 +87,67 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User>
             return -1;
         }
         return user.getId();
+    }
+
+    @Override
+    public User doLogin(String userAccount, String userPassword, HttpServletRequest request) {
+        // 1. 校验
+        if(StringUtils.isAnyBlank(userAccount, userPassword)){
+            return null;
+        }
+        if (userAccount.length() < 4){
+            return null;
+        }
+        if (userPassword.length() < 8){ //密码必须大于等于8位
+            return null;
+        }
+
+        //账户不能包含特殊字符
+        String validPattern = "[`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        if (matcher.find()) {
+            return null;
+        }
+
+        // 2. 加密 使用加密工具库 DigestUtils
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+
+        // 查询用户是否存在,返回用户信息
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        queryWrapper.eq("userPassword", encryptPassword);
+        User user = userMapper.selectOne(queryWrapper);
+        if(user == null){ // 用户不存在
+            /**
+             * Note: 可以使用注解 @Slf4j来直接使用 log对象; @Slf4j是用来做日志输出的
+             */
+            log.info("user login failed, userAccount cannot match userPassword");
+            return null;
+        }
+
+        // 3. 用户脱敏
+        User safetyUser =new User();
+        safetyUser.setId(user.getId());
+        safetyUser.setUsername(user.getUsername());
+        safetyUser.setUserAccount(user.getUserAccount());
+        safetyUser.setAvatarUrl(user.getAvatarUrl());
+        safetyUser.setGender(user.getGender());
+        safetyUser.setPhone(user.getPhone());
+        safetyUser.setEmail(user.getEmail());
+        safetyUser.setUserStatus(0);
+        safetyUser.setCreateTime(user.getCreateTime());
+
+        /**
+         * Note:
+         * 使用插件，Generate all setter with default value，快速生成类的默认值
+         */
+
+        //4. 记录用户的登录态
+        request.getSession().setAttribute(USER_LOGIN_STATE, safetyUser);
+
+        // 返回脱敏后的用户信息
+        return safetyUser;
+
     }
 }
 
